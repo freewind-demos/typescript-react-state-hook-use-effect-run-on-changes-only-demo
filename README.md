d84b120ebc74f49231dc5eac3d4219bb48a8456e
freewind <nowindlee@gmail.com>
2019-03-11 21:09:44 +0800

    demo

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-JavaScript React State Hook Demo
-=================================
+JavaScript React State Hook "useEffect" Run in Necessary Demo
+=============================================================
 
-React `16.8`中引入了`State Hook`，可以让我们以Function的形式定义组件，并且处理与state相关的操作。
-
-由于它对每一个state直接暴露出了相应的getter/setter，使用起来很方便。
-
+由于默认情况下`useEffect`将在每一次render前后都会执行，有可能造成性能问题，所以它提供了一种方式，让我们来控制其执行。
+具体办法是传入一个数组，只有当里面的值发生了变化，才会执行。
 
 ```
 npm install
--- hello.tsx
+++ hello.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {useState} from "react";
 
 export default function Hello() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log('Component is rendered, count:', count)
+    return () => {
+      console.log('Clean up this component, count: ', count)
+    }
+  }, [(count - count % 3) / 3])
+
   return (
     <div>
       <p>You clicked {count} times</p>
